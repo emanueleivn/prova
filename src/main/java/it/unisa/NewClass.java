aa41001201df503435e8525162a1752b0aecc283
emanueleivn <e.iovane@studenti.unisa.it>
2024-03-07 15:23:36 +0100

    aggiunta metodi a NewClass

--- src/main/java/it/unisa/NewClass.java
+++ src/main/java/it/unisa/NewClass.java
@@ -1,5 +1,22 @@
 package it.unisa;
 
 public class NewClass {
-
+public NewClass() {
+	// TODO Auto-generated constructor stub
+}
+@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
